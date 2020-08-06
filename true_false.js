082d2be4ba86865aa83833f0b65c3c2ef7b5c178
masud309060 <masud306090@gmail.com>
2020-08-06 23:44:39 +0600

    true false condition again doen

---
+++ true_false.js
@@ -0,0 +1,18 @@
+// #### Falsy ####
+// 0 (for number)
+// "" (emty string for string)
+// undefined 
+// null
+// NaN (NOt a Number)
+// false
+
+// #### Truthy #### 
+//"0", " ", [], {} ,"false"
+
+let name = "false";
+if(name){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
