b95672aa6d61478fe81603fab82b7186f850d1f2
masud309060 <masud306090@gmail.com>
2020-08-07 00:21:26 +0600

    null vs undefined is clear

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+// let pakhi;
+// console.log(pakhi); //undefined
+
+/* function add(num1, num2){
+    console.log(num1 + num2);
+    return //return na dile ba kake return krbo na dewa thakleo undefined 
+}
+const result = add(15, 18);
+console.log(result); // undefined */
+
+/* function add(val1,val2){
+    console.log(val1 , val2);
+}
+const result = add(15); // value isn't exist for val2
+console.log(result); */
+
+/* const premik = {name: "Smart Pola", phone: 53638};
+console.log(premik.gfName); // undefined // jokhon object e value na pawa jabe 
+ */
+
+//  let fun = undefined;
+//  console.log(fun); 
+
+
+let ages = [14, 16, 19, 20];
+console.log(ages[12]); //undefined bcz 12th term value is not here
+
+// null means not existed ;
+// or value is empty; 
--- true_false.js
+++ true_false.js
@@ -9,8 +9,8 @@
 // #### Truthy #### 
 //"0", " ", [], {} ,"false"
 
-let name = "false";
-if(name){
+let age = 0;
+if(age || age == 0){
     console.log("condition is true");
 }
 else {
